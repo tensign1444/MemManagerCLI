98c95bec2c9305c3b7fb67ef1ee34882481c0273
Tanner Ensign <tensign1444@gmail.com>
2023-04-04 10:22:53 -0700

    Fixed FreeMemoryLocation

--- main.c
+++ main.c
@@ -2,21 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include "MemoryManager/Include/MemoryManager.h"
+#include "MemoryManager/DataStructures/Include/LinkedList.h"
 
 void print_help() {
     printf("Commands:\n");
-    printf("  help      Display this help message\n");
-    printf("  hello     Greet the user with a slight intro\n");
-    printf("  malloc    Allocate memory on the created memory manager.\n");
-    printf("  free      free memory on the created memory manager.\n");
-    printf("  dump      dump the memory manager to the console.\n");
-    printf("  quit      quit the program\n");
+    printf("  help         Display this help message\n");
+    printf("  hello        Greet the user with a slight intro\n");
+    printf("  malloc {0}   Allocate memory on the created memory manager.\n");
+    printf("  free {0}     free memory on the created memory manager.\n");
+    printf("  dump         dump the memory manager to the console.\n");
+    printf("  quit         quit the program\n");
 }
 
 
 int main() {
     char command[100];
-
+    LIST *memoryManager = initMemory(30000);
     while (1) {
         printf(">: ");
         fgets(command, 100, stdin);
@@ -27,7 +28,7 @@ int main() {
                        "memory manager will manage your memory of applications running.\n");
         }
         else if (strcmp(command, "free") == 0) {
-            freeNode(NODE* H); // need to get user input
+            freeNode(h); // need to get user input
         }
         else if (strcmp(command, "malloc") == 0) {
             Malloc(0x1000);
@@ -47,5 +48,3 @@ int main() {
 
     return 0;
 }
-
-re
