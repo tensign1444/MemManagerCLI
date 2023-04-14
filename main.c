e54584d87aa9889b6579607127b8a4d0d471a05a
tensign1444 <tensign1444@gmail.com>
2023-04-14 13:27:00 -0700

    Update

--- main.c
+++ main.c
@@ -2,49 +2,66 @@
 #include <stdlib.h>
 #include <string.h>
 #include "MemoryManager/Include/MemoryManager.h"
-#include "MemoryManager/DataStructures/Include/LinkedList.h"
 
 void print_help() {
     printf("Commands:\n");
-    printf("  help         Display this help message\n");
-    printf("  hello        Greet the user with a slight intro\n");
-    printf("  malloc {0}   Allocate memory on the created memory manager.\n");
-    printf("  free {0}     free memory on the created memory manager.\n");
-    printf("  dump         dump the memory manager to the console.\n");
-    printf("  quit         quit the program\n");
+    printf("  help      Display this help message\n");
+    printf("  hello     Greet the user with a slight intro\n");
+    printf("  malloc    Allocate memory on the created memory manager.\n");
+    printf("  free      free memory on the created memory manager.\n");
+    printf("  dump      dump the memory manager to the console.\n");
+    printf("  quit      quit the program\n");
 }
+
+
 
 
 int main() {
     char command[100];
-    LIST *memoryManager = initMemory(30000);
-    NODE *tempHolder;
+    LIST *memoryManager = initMemory(30000, false);
+    NODE *p;
+
     while (1) {
+        setvbuf(stdout, NULL, _IONBF, 0);
         printf(">: ");
         fgets(command, 100, stdin);
         command[strcspn(command, "\n")] = 0; // remove trailing newline
 
-        if (strcmp(command, "hello") == 0) {
-                printf("Hello, I am a memory manager simulation. I can show you how your operating systems\n "
-                       "memory manager will manage your memory of applications running.\n");
+        char cmd[20];
+        sscanf(command, "%s", cmd); // extract first word
+
+        char* token = strtok(command, " ");
+        token = strtok(NULL, " ");
+
+        char* endptr;
+        NODE *ptr;
+        size_t second_arg;
+
+        if (strcmp(cmd, "hello") == 0) {
+            printf("Hello, I am a memory manager simulation. I can show you how your operating systems\n "
+                   "memory manager will manage your memory of applications running.\n");
         }
-        else if (strcmp(command, "free") == 0) {
-            freeMemoryLocation(memoryManager,h); // need to get user input
+        else if (strcmp(cmd, "free") == 0) {
+            ptr = (NODE*)strtoul(token, &endptr, 16);
+            freeMemoryLocation(memoryManager, ptr);
+            DumpMemoryList(memoryManager);
         }
-        else if (strcmp(command, "malloc") == 0) {
-           tempHolder = Malloc(memoryManager,0x1000);
-           printf("Memory Address: %p", tempHolder);
+        else if (strcmp(cmd, "malloc") == 0) {
+            second_arg = strtoul(token, &endptr, 10);
+            printf("0x%p\n", Malloc(memoryManager,second_arg));
+            DumpMemoryList(memoryManager);
         }
-        else if (strcmp(command, "dump") == 0) {
-            DumpMemoryList();
+        else if (strcmp(cmd, "dump") == 0) {
+            DumpMemoryList(memoryManager);
         }
-        else if (strcmp(command, "help") == 0) {
+        else if (strcmp(cmd, "help") == 0) {
             print_help();
         }
-        else if (strcmp(command, "quit") == 0) {
+        else if (strcmp(cmd, "quit") == 0) {
+            freeMemory(memoryManager);
             break;
         } else {
-            printf("Unknown command: %s\n", command);
+            printf("Unknown command: %s\n", cmd);
         }
     }
 
